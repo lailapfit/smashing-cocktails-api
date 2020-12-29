468d053f8a53c2f7bf41d4430950f18a5d9f9f82
Laila Fitriana <lailafitriana@Lailas-MBP.attlocal.net>
2020-12-28 20:47:29 -0500

    Updated database url

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL:
-      process.env.DATABASE_URL || 'postgres://noprdastxecfkk:904e8a14ff25714e11351073c3c6014f6472fdc02f71749eb0d3367e4d7ba5a9@ec2-54-211-210-149.compute-1.amazonaws.com:5432/dbehsg9sgqo3s8',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://noprdastxecfkk:904e8a14ff25714e11351073c3c6014f6472fdc02f71749eb0d3367e4d7ba5a9@ec2-54-211-210-149.compute-1.amazonaws.com:5432/dbehsg9sgqo3s8'
+      process.env.DATABASE_URL || 'postgres://sskvnbwvifstqu:efce5156cd8fcd06891369edacbd2b767c15aa7360e27110e8ed6f47ff65faf8@ec2-54-144-109-253.compute-1.amazonaws.com:5432/d4crbe3nlmeg06',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://sskvnbwvifstqu:efce5156cd8fcd06891369edacbd2b767c15aa7360e27110e8ed6f47ff65faf8@ec2-54-144-109-253.compute-1.amazonaws.com:5432/d4crbe3nlmeg06'
   }
